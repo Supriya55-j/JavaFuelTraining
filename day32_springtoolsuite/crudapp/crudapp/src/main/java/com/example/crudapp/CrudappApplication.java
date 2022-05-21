a8a0c9978bc39947a74b0b0f1ad205519c386954
Supriya55-j <103848447+Supriya55-j@users.noreply.github.com>
2022-05-21 15:37:21 +0530

    Update CrudappApplication.java

--- day32_springtoolsuite/crudapp/crudapp/src/main/java/com/example/crudapp/CrudappApplication.java
+++ day32_springtoolsuite/crudapp/crudapp/src/main/java/com/example/crudapp/CrudappApplication.java
@@ -1,5 +1,8 @@
-package com.example.crudapp;
 
+
+
+package com.example.crudapp;
+import java.util.List;
 import java.util.Optional;
 
 //import java.util.*;
@@ -7,11 +10,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-import antlr.collections.List;
-@SpringBootApplication
-public class CrudappApplication {
 
-	public static void main(String[] args) {
+@SpringBootApplication
+public class CrudappApplication
+{
+
+	public static void main(String[] args) 
+	{
 		ApplicationContext context;
 		context = SpringApplication.run(CrudappApplication.class, args);
 		StudentRepository stud;
@@ -24,34 +29,44 @@ public class CrudappApplication {
 		
 		//update
 		Optional<Student> op;
-		op=stud.findById(200);
+		op=stud.findById(900);
 		Student st2=op.get();
 		System.out.println(st2.getId());
 	System.out.println(st2.getName());
 	st2.setName("demo123");
      stud.save(st2);
  	System.out.println("record update");
-	
+/*	
 	
 	//delete
-		Optional<Student> op;
-		op=stud.findById(300);
+		Optional<Student> op1;
+		op1=stud.findById(300);
 		//Student st3=op1.get();
-		Student st3=op.get();
+		Student st3=op1.get();
 		stud.delete(st2);
-	System.out.println("record deleted ");
-	
-	
-	/*List<Student>studlist;	
+	System.out.println("record deleted ");*/
+
+	//delete
+	Optional<Student> op1;
+	op1=stud.findById(900);
+	//Student st3=op1.get();
+	Student st3=op1.get();
+	stud.delete(st2);
+System.out.println("record deleted ");
+
+	List<Student>studlist;	
+	studlist = stud.findAll();
+	for(Student st:studlist)
+		{
+			System.out.println(st.getId()+" "+st.getName());
+					
+		}/*
+	List<Student>studlist;	
 	studlist = stud.findAll();
 	for(Student st:studlist)
 		{
 			System.out.println(st.getId());
 			System.out.println(st.getName());
-				
-		}
-	*/
-		
+				}*/
 	}
-
-}
+}	
